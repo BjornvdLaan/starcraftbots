700f059d6103e666d1182cb0a3a6125f80be9711
SebasJoosten <sebasjoosten@gmail.com>
2016-12-14 12:55:55 +0100

    basic setup probe bot

---
+++ Group7Destroyer/ProbeBot/ProbeBot.pl
@@ -0,0 +1,19 @@
+:- dynamic
+state/1,
+mineralLoc/4,
+vespeneLoc/5,
+constructLoc/3,
+capacity/2,
+workerActivity/2,
+minerals/1,
+gas/1,
+unitAmount/2,
+self/2.
+
+% The cost of declared buildings (for every new building add the cost here)
+	cost("Protoss Pylon", 100, 0).
+	cost("Protoss Nexus", 400, 0).
+	cost("Protoss Gateway", 150, 0).
+	% ADDED
+        cost("Protoss Assimilator", 100, 0).
+        cost("Protoss Cybernetics Core", 200, 0).
